78f10b3043daa01878bb16e68caea0e17f849040
running1017 <R20979@kajima.co.jp>
2019-07-26 14:29:02 +0900

    first commit to Github

--- rsa_GUIbox.py
+++ rsa_GUIbox.py
@@ -5,42 +5,24 @@ Created on Thu Jul  4 08:20:26 2019
 @author: R20979
 """
 
-import rsa_tools
+import rsa_tools_obj
 import tkinter as tk
 from tkinter import ttk
-import re
 
-class Init_key(ttk.Frame):
+class Main_Window(ttk.Notebook):
     def __init__(self, master=None):
         super().__init__(master)
-        self.create_widgets()
-        self.my_key = None
 
     def create_widgets(self):
-        ttk.Label(text='key').grid(row=0, column=0, sticky='w')
-        self.editbox = ttk.Entry()
-        self.editbox.grid(row=1, column=0, columnspan=2, sticky='w')
-        ttk.Button(text='登録', command=self.button_clicked).grid()
-        self.grid(row=2, column=0, sticky='w')
+        
 
-    def button_clicked(self):
-        p = r'\(\d+[,|\n| ]+\d+\)'
-        key = self.editbox.get()
-        m = re.match(p, key)
-        if m and len(key)==m.end():
-            self.my_key = self.str2tuple(key)
-            self.grid_forget()
-            self.destroy()
-        else:
-            self.editbox.delete(0, tk.END)
-            print("not match")
+class Key_Tab(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
 
-    def str2tuple(self, string_key):
-        str_list = re.split('[,|\n| ]+', string_key.strip('()[]'))
-        return tuple(map(int, str_list))
 
 if __name__ =='__main__':
     root = tk.Tk()
-    root.title('あああ')
-    Init_key(root)
+    root.title('main job')
+    Main_Window(root)
     root.mainloop()
